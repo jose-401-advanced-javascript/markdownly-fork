c3b7993f9316988c8bbb3bc1f9a96f99399c7d4f
Jose <j.a.ojeda@live.com>
2019-11-13 15:59:11 -0800

    started styling tabs

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Tab from './Tab';
+import styles from './Tabs.css';
 
-const Tabs = ({ tabs }) => {
+const Tabs = ({ tabs, handleClick }) => {
   const tabElements = tabs.map((tab, i) => {
     return (
       <li key={i}>
-        <Tab {...tab} />
+        <Tab title={tab} handleClick={handleClick} />
       </li>
     );
   });
   return (
-    <ul>
+    <ul className={styles.Tabs}>
       {tabElements}
     </ul>
   );
 };
 
 Tabs.propTypes = {
-  tabs: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string,
-    handleClick: PropTypes.func
-  }))
+  tabs: PropTypes.arrayOf(PropTypes.string),
+  handleClick: PropTypes.func
 };
 
 export default Tabs;
