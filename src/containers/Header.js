7836798da8ed1cc3d733c7e5b7977af5798fbff3
Jose <j.a.ojeda@live.com>
2019-11-13 15:19:38 -0800

    started working on Header container

---
+++ src/containers/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Header = ({}) => {
+
+};
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+const DocumentContainer = connect(
+ 
+)
