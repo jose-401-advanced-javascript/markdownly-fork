09f56d37ae56e039777e233bab3cea764b797e32
Jose <j.a.ojeda@live.com>
2019-11-15 14:22:35 -0800

    refactored Document.js with hooks

--- src/containers/Document.js
+++ src/containers/Document.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
 import Preview from '../components/markdown/Preview';
 import Editor from '../components/markdown/Editor';
 import styles from './Document.css';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import { updateMarkdown } from '../actions/documentActions';
 import { getMarkdown } from '../selectors/documentSelectors';
 
-const Document = ({ markdown, updateMarkdown }) => {
+export default Document = () => {
+  const markdown = useSelector(getMarkdown);
+  const dispatch = useDispatch();
+  const changeMarkdown = ({ target }) => dispatch(updateMarkdown(target.value));
   return (
     <>
         <div className={styles.Document}>
-          <Editor markdown={markdown} updateMarkdown={updateMarkdown} />
+          <Editor markdown={markdown} updateMarkdown={changeMarkdown} />
           <Preview markdown={markdown} />
         </div>
       </>
@@ -22,20 +25,3 @@ Document.propTypes = {
   markdown: PropTypes.string,
   updateMarkdown: PropTypes.func.isRequired
 };
-
-const mapStateToProps = state => ({
-  markdown: getMarkdown(state)
-});
-
-const mapDispatchToProps = dispatch => ({
-  updateMarkdown({ target }) {
-    dispatch(updateMarkdown(target.value));
-  }
-});
-
-const DocumentContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Document);
-
-export default DocumentContainer;
